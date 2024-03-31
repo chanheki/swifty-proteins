367f3cdc4617c6b1560bb3045d2c90ead67083df
chanhihi <chanhihi55@gmail.com>
2024-04-01 01:35:52 +0900

    feat: init Core layer

---
+++ Projects/Core/Sources/CoreExport.swift
@@ -0,0 +1,9 @@
+//
+//  CoreExport.swift
+//  Core
+//
+//  Created by 김찬희 on 2024/04/01.
+//
+
+@_exported import CoreNetwork
+@_exported import CoreNetworkInterface
---
+++ Projects/Core/Sources/CoreRoot.swift
@@ -0,0 +1,8 @@
+//
+//  CoreRoot.swift
+//  Core
+//
+//  Created by 김찬희 on 2024/04/01.
+//
+
+// CoreRoot
