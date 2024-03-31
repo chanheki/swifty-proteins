d1a775026757393beacc1f57ddbb746c92e3b7e5
chanhihi <chanhihi55@gmail.com>
2024-04-01 01:35:14 +0900

    feat: init Domain layer

---
+++ Projects/Domain/Sources/DomainExport.swift
@@ -0,0 +1,11 @@
+//
+//  DomainExport.swift
+//  Domain
+//
+//  Created by 김찬희 on 2024/04/01.
+//
+
+@_exported import DomainProteins
+@_exported import DomainProteinsInterface
+@_exported import DomainBiometric
+@_exported import DomainBiometricInterface
