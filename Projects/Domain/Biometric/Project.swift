70d1824736eeb47a065d3cfd3a63b4ac2272f057
chanhihi <chanhihi55@gmail.com>
2024-04-01 01:34:55 +0900

    feat: add biometric in Domain

---
+++ Projects/Domain/Biometric/Project.swift
@@ -0,0 +1,39 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: ModulePath.Domain.name+ModulePath.Domain.Biometric.rawValue,
+    targets: [    
+        .domain(
+            interface: .Biometric,
+            factory: .init()
+        ),
+        .domain(
+            implements: .Biometric,
+            factory: .init(
+                dependencies: [
+                    .domain(interface: .Biometric)
+                ]
+            )
+        ),
+    
+        .domain(
+            testing: .Biometric,
+            factory: .init(
+                dependencies: [
+                    .domain(interface: .Biometric)
+                ]
+            )
+        ),
+        .domain(
+            tests: .Biometric,
+            factory: .init(
+                dependencies: [
+                    .domain(testing: .Biometric)
+                ]
+            )
+        ),
+
+    ]
+)
