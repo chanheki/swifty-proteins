b13891731816d320dc5a4fa997784f12dc89c645
chanhihi <chanhihi55@gmail.com>
2024-04-01 01:36:11 +0900

    feat: init App layer

---
+++ Projects/App/Project.swift
@@ -0,0 +1,35 @@
+import ProjectDescriptionHelpers
+import ProjectDescription
+import DependencyPlugin
+
+let targets: [Target] = [
+    .app(
+        implements: .IOS,
+        factory: .init(
+            infoPlist: .extendingDefault(
+                with: [
+                    "UILaunchStoryboardName": "LaunchScreen.storyboard",
+                    "UIApplicationSceneManifest": [
+                        "UIApplicationSupportsMultipleScenes": false,
+                        "UISceneConfigurations": [
+                            "UIWindowSceneSessionRoleApplication": [
+                                [
+                                    "UISceneConfigurationName": "Default Configuration",
+                                    "UISceneDelegateClassName": "$(PRODUCT_MODULE_NAME).SceneDelegate"
+                                ],
+                            ]
+                        ]
+                    ],
+                ]
+                ),
+            dependencies: [
+                .feature
+            ]
+        )
+    ),
+]
+
+let project: Project = .makeModule(
+    name: "SwiftyProteins",
+    targets: targets
+)
