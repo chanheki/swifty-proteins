b13a7ecd28004dd6c2e3e37582b47c18e145f000
chanhihi <chanhihi55@gmail.com>
2024-04-01 01:32:22 +0900

    feat: init shared layer

---
+++ Projects/Shared/Sources/SharedExport.swift
@@ -0,0 +1,10 @@
+//
+//  SharedRoot.swift
+//  Shared
+//
+//  Created by 김찬희 on 2024/04/01.
+//
+
+@_exported import SharedExtensions
+@_exported import SharedExtensionsInterface
+@_exported import SharedDesignSystem
