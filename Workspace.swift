44fdc4d60b1389f69c15981d2936a03fa3c1afa3
chanhihi <chanhihi55@gmail.com>
2024-04-01 01:24:33 +0900

    feat: root

---
+++ Workspace.swift
@@ -0,0 +1,13 @@
+//
+//  Workspace.swift
+//  Packages
+//
+//  Created by Chan on 3/29/24.
+//
+
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "SwiftyProteins",
+    projects: ["Projects/*"]
+)
