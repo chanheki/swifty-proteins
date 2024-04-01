28c9e9c55b0c944fc78cb81ed060802985651b34
chanhihi <chanhihi55@gmail.com>
2024-04-01 19:05:40 +0900

    feat: scene main logic

--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -7,17 +7,35 @@
 
 import UIKit
 
-import FeatureAuth
+import Feature
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-    
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
+        
+        // 앱 진입 Launch Screen 으로 변경해줘야함
+        let mainViewController = UIViewController()
+        mainViewController.view.backgroundColor = .blue
+        
+        window?.rootViewController = mainViewController
+        window?.makeKeyAndVisible()
+        
         let biometricFlow = BiometricAuthenticationFlow(window: window)
-        biometricFlow.start()
+        
+        biometricFlow.start { success, error in
+            DispatchQueue.main.async {
+                if success {
+                    // 앱 정상 실행 Flow Feature
+                    biometricFlow.showMainViewController()
+                } else {
+                    // 앱 꺼짐 혹은 재시도 Flow Feature
+                    biometricFlow.showFailureViewController(error: error)
+                }
+            }
+        }
     }
 }
