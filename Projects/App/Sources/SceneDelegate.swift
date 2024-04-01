10c3987f41a2a56fd3aaa448489a2a73ab4d422e
chanhihi <chanhihi55@gmail.com>
2024-04-01 16:47:33 +0900

    feat: app

--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -6,24 +6,18 @@
 //
 
 import UIKit
-import LocalAuthentication
-import LocalAuthenticationEmbeddedUI
+
+import FeatureAuth
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        
-        
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-
-        let viewController = UIViewController()
-        viewController.view.backgroundColor = .orange
         
-        window?.rootViewController = viewController
-        window?.makeKeyAndVisible()
+        window = UIWindow(windowScene: windowScene)
+        let biometricFlow = BiometricAuthenticationFlow(window: window)
+        biometricFlow.start()
     }
 }
