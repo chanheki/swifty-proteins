1213d01097b25e24f6372669f6ff1b7d3f24bf2b
chanhihi <chanhihi55@gmail.com>
2024-04-01 19:06:20 +0900

    feat: add core authentication for biometric

---
+++ Projects/Core/Authentication/Project.swift
@@ -0,0 +1,39 @@
+import ProjectDescription
+import ProjectDescriptionHelpers
+import DependencyPlugin
+
+let project = Project.makeModule(
+    name: ModulePath.Core.name+ModulePath.Core.Authentication.rawValue,
+    targets: [    
+        .core(
+            interface: .Authentication,
+            factory: .init()
+        ),
+        .core(
+            implements: .Authentication,
+            factory: .init(
+                dependencies: [
+                    .core(interface: .Authentication)
+                ]
+            )
+        ),
+    
+        .core(
+            testing: .Authentication,
+            factory: .init(
+                dependencies: [
+                    .core(interface: .Authentication)
+                ]
+            )
+        ),
+        .core(
+            tests: .Authentication,
+            factory: .init(
+                dependencies: [
+                    .core(testing: .Authentication)
+                ]
+            )
+        ),
+
+    ]
+)
