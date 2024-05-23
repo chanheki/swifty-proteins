575bfb96c46f3f33a339e5c79772fac7aeae3a5f
chanhihi <chanhihi55@gmail.com>
2024-05-23 22:37:37 +0900

    refactor: separatine SceneDelegate Responsibilities

--- Projects/App/Sources/SceneDelegate.swift
+++ Projects/App/Sources/SceneDelegate.swift
@@ -8,34 +8,75 @@
 import UIKit
 
 import Feature
+import SharedCommonUI
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
+    var coverViewManager: CoverViewManager?
+    var biometricFlow: BiometricAuthenticationFlow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(windowScene: windowScene)
         
-        // 앱 진입 Launch Screen 으로 변경해줘야함
-        let mainViewController = UIViewController()
-        mainViewController.view.backgroundColor = .blue
+        // 프로틴 리스트를 보여줄 메인 뷰 컨트롤러 생성
+        let mainViewController = ProteinsListViewController()
+        mainViewController.view.backgroundColor = .white // 배경색 설정
         
-        window?.rootViewController = mainViewController
+        // 메인 뷰 컨트롤러를 네비게이션 컨트롤러의 루트로 설정
+        let navigationController = UINavigationController(rootViewController: mainViewController)
+        
+        // 네비게이션 바 설정
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.isTranslucent = true
+        navigationController.navigationBar.barTintColor = .blue
+        navigationController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        mainViewController.title = "Swifty Proteins"
+        
+        window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
         
-        let biometricFlow = BiometricAuthenticationFlow(window: window)
+        // 커버 뷰 관리 및 바이오메트릭 인증 플로우 초기화
+        coverViewManager = CoverViewManager(window: window)
+        biometricFlow = BiometricAuthenticationFlow(window: window)
         
-        biometricFlow.start { success, error in
-            DispatchQueue.main.async {
-                if success {
-                    // 앱 정상 실행 Flow Feature
-                    biometricFlow.showMainViewController()
-                } else {
-                    // 앱 꺼짐 혹은 재시도 Flow Feature
-                    biometricFlow.showFailureViewController(error: error)
-                }
+        // 처음 앱 실행 시 커버 뷰를 추가 및 인증 시작
+        coverViewManager?.addCoverView()
+        authenticateUser()
+    }
+    
+    private func authenticateUser() {
+        biometricFlow?.start { [weak self] success, error in
+            if success {
+                self?.coverViewManager?.removeCoverView()
+            } else {
+                self?.biometricFlow?.showFailureViewController(error: error)
             }
+        }
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        // 홈 버튼 두 번 눌러 앱이 비활성화될 때 커버 뷰 추가
+        coverViewManager?.addCoverView()
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // 대기 모드 진입 시 커버 뷰 추가
+        coverViewManager?.addCoverView()
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // 다시 활성화될 때 인증 로직 호출
+        if coverViewManager?.hasCoverView() == true {
+            authenticateUser()
+        }
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // 다시 활성화될 때 인증 로직 호출
+        if coverViewManager?.hasCoverView() == true {
+            authenticateUser()
         }
     }
 }
