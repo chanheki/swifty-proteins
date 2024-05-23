2aa2aca978c9226ea38dc7d8a7ecfe0a66ff3dfc
chanhihi <chanhihi55@gmail.com>
2024-05-24 04:10:38 +0900

    feat: proteins MVVM구조를 바인딩합니다.

--- Projects/Feature/Proteins/Sources/ProteinsListViewController.swift
+++ Projects/Feature/Proteins/Sources/ProteinsListViewController.swift
@@ -6,31 +6,54 @@
 //
 
 import UIKit
+import Combine
 
+import DomainProteins
 import Shared
 
 public final class ProteinsListViewController: BaseViewController<ProteinsListView>, UISearchResultsUpdating {
     
+    private var viewModel: LigandsViewModel?
+    private var cancellables: Set<AnyCancellable> = []
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        bindViewModel()
+    }
+
+    private func bindViewModel() {
+        guard let proteinsView = self.contentView as? ProteinsListView else {
+            return
+        }
+        
+        if let viewModel = viewModel {
+            viewModel.$ligandsBySection
+                .receive(on: RunLoop.main)
+                .sink { _ in
+                    proteinsView.proteinsTableView.reloadData()
+                }
+                .store(in: &cancellables)
+        }
+    }
+    
     public override func setupProperty() {
+        if let proteinListView = self.contentView as? ProteinsListView {
+            viewModel = proteinListView.viewModel
+        }
+        
         self.view.backgroundColor = .backgroundColor
         self.title = "Swifty Proteins"
         
-        setNavigationBarHidden(true)
-        setNavigationBarBackgroundColor(.proteins)
-    }
-    
-    public func initialView() -> UIViewController {
-        // 현재 프로틴 리스트 컨트롤러를 네비게이션 컨트롤러의 루트로 설정
-        let initialViewController = self.setupSearchBar()
+        self.setNavigationBarHidden(true)
+        self.setNavigationBarBackgroundColor(.proteins)
         
-        return initialViewController
+        self.configureSearchController(delegate: self, navigationItem: navigationItem)
+        
+        self.definesPresentationContext = true
     }
     
+    // SearchBar 초기화
     private func setupSearchBar() -> UIViewController {
-        navigationBar.configureSearchController(delegate: self, navigationItem: navigationItem)
-        
-        definesPresentationContext = true
-        
         let searchBarController = UINavigationController(rootViewController: self)
         
         // 네비게이션 바 설정
@@ -41,15 +64,27 @@ public final class ProteinsListViewController: BaseViewController<ProteinsListVi
         searchBarController.navigationBar.backgroundColor = .backgroundColor
         return searchBarController
     }
+    
+    // 현재 프로틴 리스트 컨트롤러를 네비게이션 컨트롤러의 루트로 설정
+    public func initialView() -> UIViewController {
+        return self.setupSearchBar()
+    }
+    
+    // SearchController 초기화
+    public func configureSearchController(delegate: UISearchResultsUpdating, navigationItem: UINavigationItem) {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = delegate
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search Ligands"
+        
+        navigationItem.searchController = searchController
+        navigationItem.hidesSearchBarWhenScrolling = false
+    }
 }
 
-// UISearchResultsUpdating 프로토콜을 확장하여 검색 결과 업데이트 로직을 구현합니다.
+// 검색 결과 업데이트 로직
 extension ProteinsListViewController {
     public func updateSearchResults(for searchController: UISearchController) {
-        guard let searchText = searchController.searchBar.text, !searchText.isEmpty else {
-            // 검색 텍스트가 없는 경우의 처리 로직 (예: 모든 데이터를 다시 표시)
-            return
-        }
-        
+        viewModel?.searchLigands(with: searchController.searchBar.text ?? "")
     }
 }
