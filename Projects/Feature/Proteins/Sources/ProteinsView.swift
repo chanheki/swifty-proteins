1175b1409eb867115d3e514954122df7897e9e8b
chanhihi <chanhihi55@gmail.com>
2024-05-30 03:47:51 +0900

    feat: MVP protein view

--- Projects/Feature/Proteins/Sources/ProteinsView.swift
+++ Projects/Feature/Proteins/Sources/ProteinsView.swift
@@ -6,7 +6,35 @@
 //
 
 import UIKit
+import SceneKit
 
 open class ProteinsView: UIView {
+    let sceneView: SCNView = {
+        let view = SCNView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.autoenablesDefaultLighting = true
+        view.allowsCameraControl = true
+        return view
+    }()
     
+    override public init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required public init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    private func setupView() {
+        addSubview(sceneView)
+        
+        NSLayoutConstraint.activate([
+            sceneView.topAnchor.constraint(equalTo: topAnchor, constant: 20),
+            sceneView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            sceneView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            sceneView.bottomAnchor.constraint(equalTo: bottomAnchor)
+        ])
+    }
 }
