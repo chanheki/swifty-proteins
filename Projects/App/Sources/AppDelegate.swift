f99bcb958e0a27038e00c0fe2ad8dbb2d01e2cb4
chanhihi <chanhihi55@gmail.com>
2024-06-03 19:19:41 +0900

    fix: coredata core레이어로 분리

--- Projects/App/Sources/AppDelegate.swift
+++ Projects/App/Sources/AppDelegate.swift
@@ -36,14 +36,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
     
-    lazy var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "UserModel")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
 }
