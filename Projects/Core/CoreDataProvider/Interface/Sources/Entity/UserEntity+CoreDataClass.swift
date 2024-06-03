c19af3b1b895a0b4e00b566416d00fa1268692d9
chanhihi <chanhihi55@gmail.com>
2024-06-03 19:16:51 +0900

    feat: add user entity

---
+++ Projects/Core/CoreDataProvider/Interface/Sources/Entity/UserEntity+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  UserEntity+CoreDataClass.swift
+//  
+//
+//  Created by Chan on 6/2/24.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(UserEntity)
+public class UserEntity: NSManagedObject {
+
+}
