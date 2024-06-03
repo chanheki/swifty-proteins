f59e2b17b1869b438e49caeee09d1c557b50253b
chanhihi <chanhihi55@gmail.com>
2024-06-03 19:22:45 +0900

    feat: login view controller feature 레이어로 분리

---
+++ Projects/Feature/Authentication/Interface/Sources/LoginViewControllerDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  LoginViewControllerDelegate.swift
+//  FeatureAuthentication
+//
+//  Created by Chan on 6/2/24.
+//
+
+// 로그인 성공 후 콜백 처리를 위한 프로토콜
+public protocol LoginViewControllerDelegate: AnyObject {
+    func loginDidFinish(success: Bool, error: Error?)
+}
