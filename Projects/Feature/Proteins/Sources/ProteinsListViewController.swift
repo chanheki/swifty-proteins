ab4d5adaa525d63c41153b8edb601c32f7866bc6
chanhihi <chanhihi55@gmail.com>
2024-06-05 19:31:18 +0900

    feat: scrollview, navigation bar 최적화

--- Projects/Feature/Proteins/Sources/ProteinsListViewController.swift
+++ Projects/Feature/Proteins/Sources/ProteinsListViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Combine
 
+import FeatureProteinsInterface
 import DomainProteins
 import SharedCommonUI
 import SharedExtensions
@@ -22,12 +23,25 @@ public final class ProteinsListViewController: BaseViewController<ProteinsListVi
         bindViewModel()
     }
     
+    public override func viewWillAppear(_ animated: Bool) {
+        self.title = "Swifty Proteins"
+    }
+    
     private func bindViewModel() {
         guard let proteinsView = self.contentView as? ProteinsListView else {
             return
         }
         
         if let viewModel = viewModel {
+            // filteredLigands 바인딩 추가
+            viewModel.$filteredLigands
+                .receive(on: RunLoop.main)
+                .sink { [weak self] _ in
+                    guard let self = self else { return }
+                    proteinsView.proteinsTableView.reloadData()
+                }
+                .store(in: &cancellables)
+            
             viewModel.$ligandsBySection
                 .receive(on: RunLoop.main)
                 .sink { _ in
@@ -44,49 +58,72 @@ public final class ProteinsListViewController: BaseViewController<ProteinsListVi
         }
         
         self.view.backgroundColor = .backgroundColor
-        self.title = "Swifty Proteins"
         
         self.setNavigationBarHidden(true)
         self.setNavigationBarBackgroundColor(.proteins)
         
-        self.configureSearchController(delegate: self, navigationItem: navigationItem)
-        
         self.definesPresentationContext = true
     }
     
-    // SearchBar 초기화
-    private func setupSearchBar() -> UIViewController {
-        let searchBarController = UINavigationController(rootViewController: self)
+    // navigationbar 초기화
+    private func setupNavigationBar() -> UIViewController {
+        // Navigation Controller 생성
+        let _ = UINavigationController(rootViewController: self)
         
         // 네비게이션 바 설정
-        searchBarController.navigationBar.prefersLargeTitles = true
-        searchBarController.navigationBar.isTranslucent = true
-        searchBarController.navigationBar.tintColor = .foregroundColor
-        searchBarController.navigationBar.barTintColor = .foregroundColor
-        searchBarController.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.foregroundColor]
-        searchBarController.navigationBar.backgroundColor = .backgroundColor
-        return searchBarController
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.tintColor = .foregroundColor
+        self.navigationController?.navigationBar.barTintColor = .foregroundColor
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.foregroundColor]
+        self.navigationController?.navigationBar.backgroundColor = .backgroundColor
+        
+        // Title
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        // search
+        self.navigationController?.navigationItem.hidesSearchBarWhenScrolling = false
+        // 네비게이션바에 들어갈 search controller 설정
+        self.configureSearchController(delegate: self, navigationItem: navigationItem)
+        
+        // UserButton 생성
+        let userButton = UIButton(type: .custom)
+        userButton.setImage(UIImage(systemName: "person.crop.circle"), for: .normal)
+        userButton.tintColor = .systemBlue
+        userButton.addTarget(self, action: #selector(userButtonTapped), for: .touchUpInside)
+        
+        // UserButton을 UIBarButtonItem으로 변환
+        let userBarButtonItem = UIBarButtonItem(customView: userButton)
+        
+        // navigationItem의 rightBarButtonItem으로 설정
+        self.navigationItem.rightBarButtonItem = userBarButtonItem
+        
+        return self.navigationController!
     }
     
     // 현재 프로틴 리스트 컨트롤러를 네비게이션 컨트롤러의 루트로 설정
     public func initialView() -> UIViewController {
-        return self.setupSearchBar()
+        return self.setupNavigationBar()
     }
     
     // SearchController 초기화
     public func configureSearchController(delegate: UISearchResultsUpdating, navigationItem: UINavigationItem) {
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = delegate
-        searchController.obscuresBackgroundDuringPresentation = false
-        searchController.searchBar.placeholder = "Search Ligands"
+        let searchbar =  UISearchController(searchResultsController: nil)
+        searchbar.searchResultsUpdater = delegate
+        searchbar.obscuresBackgroundDuringPresentation = false
+        searchbar.searchBar.placeholder = "Search Ligands"
+        navigationItem.searchController = searchbar
+    }
+    
+    @objc private func userButtonTapped() {
+        let proteinsViewController = ProteinsViewController()
         
-        navigationItem.searchController = searchController
-        navigationItem.hidesSearchBarWhenScrolling = false
+        navigationController?.pushViewController(proteinsViewController, animated: true)
     }
 }
 
-// 검색 결과 업데이트 로직
 extension ProteinsListViewController {
+    // 검색 결과 업데이트 로직
     public func updateSearchResults(for searchController: UISearchController) {
         viewModel?.searchLigands(with: searchController.searchBar.text ?? "")
     }
@@ -104,11 +141,22 @@ extension ProteinsListViewController: ProteinsListTableViewDelegate {
         if let ligand = viewModel?.ligandsBySection[sectionKey]?[indexPath.row] {
             let proteinsViewController = ProteinsViewController()
             
+            self.title = ligand.identifier
             proteinsViewController.ligand = ligand
-            proteinsViewController.title = ligand.identifier
             proteinsViewController.setNavigationBarTitleLabelText(ligand.identifier)
             navigationController?.pushViewController(proteinsViewController, animated: true)
         }
     }
-
+    
+    // 스크롤 뷰 델리게이트 메서드
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let offsetY = scrollView.contentOffset.y
+        let largeTitleHeight = self.navigationController?.navigationBar.frame.height ?? 0.1
+        
+        if offsetY > largeTitleHeight {
+            self.navigationItem.largeTitleDisplayMode = .never
+        } else {
+            self.navigationItem.largeTitleDisplayMode = .always
+        }
+    }
 }
