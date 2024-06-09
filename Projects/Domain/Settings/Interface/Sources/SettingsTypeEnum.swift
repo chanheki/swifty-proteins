1771c32ec55a5f57b0ab6f31dcc51b8b92d016a3
chanhihi <chanhihi55@gmail.com>
2024-06-09 19:46:25 +0900

    feat: settings view model

---
+++ Projects/Domain/Settings/Interface/Sources/SettingsTypeEnum.swift
@@ -0,0 +1,14 @@
+//
+//  SettingsTypeEnum.swift
+//  DomainSettingsInterface
+//
+//  Created by Chan on 6/9/24.
+//
+
+public enum SettingsType {
+    case id
+    case resetPassword
+    case biometric
+    case logout
+    case unsubscribe
+}
