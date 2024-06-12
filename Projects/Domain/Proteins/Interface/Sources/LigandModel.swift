68be94be63bca4a4700b11f3c4511202eb8e1362
chanhihi <chanhihi55@gmail.com>
2024-06-12 15:24:08 +0900

    fix: ligandModel

---
+++ Projects/Domain/Proteins/Interface/Sources/LigandModel.swift
@@ -0,0 +1,14 @@
+//
+//  LigandModel.swift
+//  DomainProteinsInterface
+//
+//  Created by Chan on 6/11/24.
+//
+
+public struct LigandModel {
+    public let identifier: String
+
+    public init(identifier: String) {
+        self.identifier = identifier
+    }
+}
